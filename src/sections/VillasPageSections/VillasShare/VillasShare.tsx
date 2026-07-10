9c0741485fe18a82c07fa8e5973a30ff9fd5f91e
artem_kruzhkov <artkruzhkov@gmail.com>
2026-07-10 16:30:36 +0300

    fixed top-text in VillasShare 2 lines for all laptops and desktops

--- src/sections/VillasPageSections/VillasShare/VillasShare.tsx
+++ src/sections/VillasPageSections/VillasShare/VillasShare.tsx
@@ -29,7 +29,7 @@ export function VillasShare() {
       <div className="mx-auto max-w-8xl 2xl:max-w-[1920px]">
         {/* TOP TEXT */}
         <motion.div
-          className="px-6 lg:px-20 xl:px-10 2xl:px-[8rem] py-6 lg:py-10 2xl:py-14 text-center"
+          className="px-6 lg:px-4 xl:px-10 2xl:px-[8rem] py-6 lg:py-10 2xl:py-14 text-center flex flex-row justify-center"
           initial={{ opacity: 0, y: 30 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true, amount: 0.4 }}
@@ -40,7 +40,7 @@ export function VillasShare() {
           <AnimatePresence mode="wait">
             <motion.h2
               key={t('villasShare.title')}
-              className="font-serif text-[19px] sm:text-[23px] md:text-[28px] xl:text-[32px] 2xl:text-[36px] leading-[1.25] tracking-[-0.02rem] text-[#2C3654]"
+              className="font-serif text-[19px] sm:text-[23px] md:text-[28px] xl:text-[32px] 2xl:text-[36px] leading-[1.25] tracking-[-0.02rem] text-[#2C3654] lg:w-[992px] xl:w-[1136px] 2xl:w-[1294px]"
               initial={{ opacity: 0, y: 12 }}
               animate={{ opacity: 1, y: 0 }}
               exit={{ opacity: 0, y: -12 }}
